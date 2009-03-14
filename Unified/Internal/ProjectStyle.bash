b89626134d177fd8c45c60e3b7990638fc66f992
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2009-03-14 18:29:33 +0100

    Renamed 'Style.bash' to 'ProjectStyle.bash' to reflect the fact it only deals with project styles (eg 'iPhone'), not build styles (eg 'debug'). [2: missing file added.]

---
+++ Unified/Internal/ProjectStyle.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f "$INFINITELABS_UNIFIED_PROJECT_DIR"/ProjectStyle ]; then
+	echo "error: The project has no set style. Use Unified/SetStyle <style> to set the style." >&2
+	exit 1
+fi
+
+INFINITELABS_UNIFIED_PROJECT_STYLE="`cat "$INFINITELABS_UNIFIED_PROJECT_DIR"/ProjectStyle`"
+
+if [ "$INFINITELABS_UNIFIED_PROJECT_STYLE" == "Internal" ]; then
+	echo "error: The project has a reserved project style ('Internal'). Set it to something else using Unified/SetStyle <style>." >&2
+	exit 1
+fi
+
+if [ ! -d "$INFINITELABS_UNIFIED_DIR"/"$INFINITELABS_UNIFIED_PROJECT_STYLE" ]; then
+	echo "error: The style named '$INFINITELABS_UNIFIED_PROJECT_STYLE' is unknown." >&2
+	ILUnifiedLog directory "$INFINITELABS_UNIFIED_DIR"/"$INFINITELABS_UNIFIED_PROJECT_STYLE" not found.
+	exit 1
+fi
