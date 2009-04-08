b7fbc693091a728f227961912786a1606d373e0e
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2009-04-08 10:09:12 +0200

    Added L0Show() and UIApp to the repertoire.

--- Common/Mac/Common.h
+++ Common/Mac/Common.h
@@ -7,9 +7,11 @@
 #ifdef DEBUG
 	#define L0Log(x, ...) NSLog(@"<DEBUG: %s>: " x, __func__, ## __VA_ARGS__)
 	#define L0LogIf(cond, x, ...) do { if (cond) L0Log(x, ## __VA_ARGS__); } while (0)
+	#define L0Show(x) L0Log(@"%@", [(x) description])
 #else
 	#define L0Log(...)
 	#define L0LogIf(...)
+	#define L0Show(...)
 #endif // def DEBUG
 
 #endif // def __OBJC__
